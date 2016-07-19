0cd33aafeedef7f3823f870e547038938334c292
artem.d <artem.d@sbtech.com>
2016-07-19 17:17:03 +0300

    add Command

---
+++ BehavioralPatterns/DesignPatterns.BehavioralPatterns.Command/Commands.cs
@@ -0,0 +1,27 @@
+﻿namespace DesignPatterns.BehavioralPatterns.Command
+{
+    internal abstract class Command
+    {
+        protected Receiver receiver;
+
+        protected Command(Receiver receiver)
+        {
+            this.receiver = receiver;
+        }
+
+        public abstract void Execute();
+    }
+
+    internal class ConcreteCommand : Command
+    {
+        public ConcreteCommand(Receiver receiver)
+            : base(receiver)
+        {
+        }
+
+        public override void Execute()
+        {
+            this.receiver.Action();
+        }
+    }
+}
---
+++ BehavioralPatterns/DesignPatterns.BehavioralPatterns.Command/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace DesignPatterns.BehavioralPatterns.Command
+{
+    public sealed class Program
+    {
+        private static void Main()
+        {
+            // Create receiver, command, and invoker
+            Receiver receiver = new Receiver();
+            Command command = new ConcreteCommand(receiver);
+            Invoker invoker = new Invoker();
+
+            // Set and execute command
+            invoker.SetCommand(command);
+            invoker.ExecuteCommand();
+
+            Console.ReadKey();
+        }
+    }
+
+    internal class Receiver
+    {
+        public void Action()
+        {
+            Console.WriteLine("Called Receiver.Action()");
+        }
+    }
+
+    internal class Invoker
+    {
+        private Command command;
+
+        public void SetCommand(Command command)
+        {
+            this.command = command;
+        }
+
+        public void ExecuteCommand()
+        {
+            this.command.Execute();
+        }
+    }
+}
